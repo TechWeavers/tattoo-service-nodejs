a3e0b4df05371d6f3bdf2490b3df1d27443d12a1
hideakifukami <denys.fukami@gmail.com>
2023-09-12 16:34:26 -0300

    JawsBD

--- models/banco.js
+++ models/banco.js
@@ -1,12 +1,22 @@
 const Sequelize = require("sequelize")
-const sequelize = new Sequelize("TattooServiceBD", "root", "", {
-    host: "localhost",
-    dialect: "mysql",
-    define: {
-        timestamps: false, 
-        freezeTableName: true, 
+let sequelize
+
+if (process.env.NODE_ENV === 'production' && process.env.JAWSDB_MARIA_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_MARIA_URL, {
+      dialect: 'mysql',
+      logging: false, // Desative os logs se preferir
+    });
+  } else {
+    // Use as configurações do banco de dados local se não estiver no Heroku
+    sequelize = new Sequelize('TattooServiceBD', 'root', '', {
+      host: 'localhost',
+      dialect: 'mysql',
+      define: {
+        timestamps: false,
+        freezeTableName: true,
       },
-})
+    });
+  }
 
 module.exports = {
     Sequelize: Sequelize,
