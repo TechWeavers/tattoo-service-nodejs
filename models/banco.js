2ecea1774b9991f51bdb54b88cde0ee1e999ecfa
hideakifukami <denys.fukami@gmail.com>
2023-09-12 16:48:30 -0300

    jaws

--- models/banco.js
+++ models/banco.js
@@ -1,8 +1,20 @@
 const Sequelize = require("sequelize")
 let sequelize
+var mysql = require('mysql');
+var connection = mysql.createConnection(process.env.JAWSDB_URL);
 
-if (process.env.NODE_ENV === 'production' && process.env.JAWSDB_MARIA_URL) {
-    sequelize = new Sequelize(process.env.JAWSDB_MARIA_URL, {
+connection.connect();
+
+connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+  if (err) throw err;
+
+  console.log('The solution is: ', rows[0].solution);
+});
+
+connection.end();
+
+if (process.env.NODE_ENV === 'production' && process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL, {
       dialect: 'mysql',
       logging: false, // Desative os logs se preferir
     });
