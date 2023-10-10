46e731712528cec4d9c6238842f15c435517860d
devWeb-JP18 <jplima.dev@outlook.com>
2023-10-09 21:11:23 -0300

    comit de teste do repositório - Vizera

--- models/db.js
+++ models/db.js
@@ -1,18 +1,18 @@
 const Sequelize = require("sequelize")
-const sequelize = new Sequelize('DBTattooService', 'root', '', {
-      host: 'localhost',
-      dialect: 'mysql',
-      define: {
+const sequelize = new Sequelize('DBTattooService', 'root', 'devsolitario18', {
+    host: 'localhost',
+    dialect: 'mysql',
+    define: {
         timestamps: false,
         freezeTableName: true,
-      },
-    });
+    },
+});
 
 // Sincronize os modelos com o banco de dados
 sequelize.sync().then(() => {
-  console.log('Tabelas sincronizadas com sucesso.');
+    console.log('Tabelas sincronizadas com sucesso.');
 }).catch(err => {
-  console.error('Erro ao sincronizar tabelas:', err);
+    console.error('Erro ao sincronizar tabelas:', err);
 });
 
 module.exports = {
