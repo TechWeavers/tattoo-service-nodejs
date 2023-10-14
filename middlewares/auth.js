255b5d9f88da0862495419a9f330cbcad04f518e
devWeb-JP18 <jplima.dev@outlook.com>
2023-10-14 12:02:02 -0300

    commit teste

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,24 +4,24 @@ const tokenModule = require('../modules/token');
 
 
 module.exports = {
-    eAdmin: async (req, res, next) => {
+    eAdmin: async(req, res, next) => {
         //const authHeader = req.headers.authorization;
         //const token = authHeader.split(" ")[1];
 
         const token = tokenModule.getToken();
 
-        if(!token) {
+        if (!token) {
             return res.status(400).json({
                 erro: true,
                 mensagem: "Erro: Necessario token"
             })
         }
 
-        try{
+        try {
             const decode = await promisify(jwt.verify)(token, "J98JDASD908ML0G9ZV8ML1PI3I89S7D6F");
             req.idUsuario = decode.id;
             return next();
-        }catch(err){
+        } catch (err) {
             return res.status(400).json({
                 erro: true,
                 mensagem: "Token Invalido"
