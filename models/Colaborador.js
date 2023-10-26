3d80b5ca5a3b967d6a7069345d323e3e846743b3
devWeb-JP18 <jplima.dev@outlook.com>
2023-10-25 22:06:18 -0300

    adicionada a entidade telefone, e relação 1,N de colaborador com telefone_colaborador

--- models/Colaborador.js
+++ models/Colaborador.js
@@ -2,41 +2,43 @@ const Administrador = require("./Administrador");
 const Procedimento = require("./Procedimento");
 const Tatuador = require("./Tatuador");
 const Usuario = require("./Usuario");
+const Telefone_Colaborador = require("./Telefone_Colaborador");
 const db = require("./db")
 
 const Colaborador = db.sequelize.define('Colaborador', {
-  id_colaborador: {
-    type: db.Sequelize.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
-  },
-  nome: {
-    type: db.Sequelize.STRING,
-  },
-  cpf: {
-    type: db.Sequelize.STRING,
-  },
-  telefone: {
-    type: db.Sequelize.STRING,
-  },
-  email: {
-    type: db.Sequelize.STRING,
-  },
-  redeSocial: {
-    type: db.Sequelize.STRING,
-  },
-  tipo: {
-    type: db.Sequelize.ENUM("Tatuador", "Administrador"),
-  }
+    id_colaborador: {
+        type: db.Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    nome: {
+        type: db.Sequelize.STRING,
+    },
+    cpf: {
+        type: db.Sequelize.STRING,
+    },
+    telefone: {
+        type: db.Sequelize.STRING,
+    },
+    email: {
+        type: db.Sequelize.STRING,
+    },
+    redeSocial: {
+        type: db.Sequelize.STRING,
+    },
+    tipo: {
+        type: db.Sequelize.ENUM("Tatuador", "Administrador"),
+    }
 });
 
 Colaborador.hasOne(Usuario, { foreignKey: 'fk_colaborador' });
 Colaborador.hasOne(Procedimento, { foreignKey: 'fk_colaborador' });
 Colaborador.hasOne(Tatuador, { foreignKey: 'fk_colaborador' });
 Colaborador.hasOne(Administrador, { foreignKey: 'fk_colaborador' });
+Colaborador.hasMany(Telefone_Colaborador, { foreignKey: 'fk_colaborador' })
 
 
 
 //Colaborador.sync({ force: true })
-  
+
 module.exports = Colaborador
---
+++ models/Telefone_Colaborador.js
@@ -0,0 +1,22 @@
+const db = require("./db")
+const Colaborador = require("./Colaborador");
+
+const Telefone_Colaborador = db.sequelize.define("telefone_colaborador", {
+    id_telefone: {
+        type: db.Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    telefone: {
+        type: db.Sequelize.INTEGER,
+
+    },
+    fk_colaborador: {
+        type: db.Sequelize.INTEGER,
+
+    }
+})
+
+Telefone_Colaborador.sync({ force: true });
+
+module.exports = Telefone_Colaborador;
