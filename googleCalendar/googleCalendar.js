23cd1879f98fde4d79f97dceeb5fd871dddb561d
joao pedro <jplima.dev@outlook.com>
2023-11-27 23:40:53 -0300

    API funfando novamente

--- googleCalendar/googleCalendar.js
+++ googleCalendar/googleCalendar.js
@@ -18,7 +18,7 @@ const SCOPES = ['https://www.googleapis.com/auth/calendar'];
 // The file token.json stores the user's access and refresh tokens, and is
 // created automatically when the authorization flow completes for the first
 // time.
-const TOKEN_PATH = path.join(process.cwd(), 'token.json');
+const TOKEN_PATH = path.join(process.cwd(), 'teste-api-calendar.json');
 const CREDENTIALS_PATH = path.join(process.cwd(), 'credentials.json');
 
 /**
